47541b76c138886796be36b4831c38011b2776a2
Emmanuel Jesse <magachi.emmanuel@students.jkuat.ac.ke>
2025-04-17 11:41:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+# Create a pet instance
+my_pet = Pet("Buddy")
+
+# Interactions
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("sit")
+my_pet.train("high five")
+my_pet.show_tricks()
+my_pet.get_status()
